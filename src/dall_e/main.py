c83079d8b68f067c96f3cf39429665c51a868840
Stefan Boos <kontakt@boos.systems>
2024-04-23 20:20:58 +0200

    feat: request image from DALL-E

--- src/dall_e/main.py
+++ src/dall_e/main.py
@@ -1,6 +1,30 @@
-def hello():
-    return "Hello, world!"
+import os
+
+import requests
+from openai import OpenAI
+from dotenv import load_dotenv
+
+
+def main():
+    load_dotenv()
+    print(f"Using OpenAI API key {os.environ['OPENAI_API_KEY_NAME']}")
+
+    client = OpenAI()
+
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt="a white siamese cat",
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    print(f"Rendered image: {response.data[0].url}")
+
+    image_data = requests.get(response.data[0].url).content
+    with open("output.jpg", "wb") as file:
+        file.write(image_data)
 
 
 if __name__ == "__main__":
-    print(hello())
+    main()
--- tests/test_main.py
+++
@@ -1,5 +0,0 @@
-from dall_e.main import hello
-
-
-def test_main():
-    assert hello() == "Hello, world!"
